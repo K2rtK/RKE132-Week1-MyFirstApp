70125aa232452e24e84574dd02bff34a9bda330c
K2rt <kkursa@tktk.ee>
2023-02-05 22:32:53 +0200

    create new app

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Hello, Kärt!");
+Console.WriteLine("Enter your name:");
+string userName = userName = Console.ReadLine();
+
+Console.WriteLine("Hello" +", " + "userName" + "!");
+
+
+Console.WriteLine($"Hello, {userName} !");
